eee71466e1a6eeb2634b24347927b137e4ba773e
Praveen <spraveen593@gmail.com>
2022-08-26 03:32:37 +0530

    feat: :fire: Added Template For Front Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import { useState } from "react";
 import Input from "./Components/Input";
 import { Container } from "./Components/Container";
-import { Text } from "@chakra-ui/react";
-
+import { Text, Box, Button } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState({
+    topic: "",
+    location: "",
+  });
+
+  const handleChange = (e) => setData({ ...data, [e.target.name]: e.target.value });
+
+  console.log(data);
 
   return (
     <div className="App">
       <Container>
-        <Text my={12} fontWeight={"extrabold"} align={"center"} fontSize={[20, 30, 40]}>
+        <Text
+          my={12}
+          fontWeight={"extrabold"}
+          align={"center"}
+          fontSize={[20, 30, 40]}
+        >
           Github Search
         </Text>
-        <Input placeholder="Enter Data" />
+        <Box
+          className="mainBox"
+          borderColor={"gray.400"}
+          border={"2px"}
+          borderRadius="5"
+          p={4}
+        >
+          <Input
+            onChange={handleChange}
+            name="topic"
+            placeholder="Enter Topic"
+          />
+          <Input
+            onChange={handleChange}
+            name="location"
+            placeholder="Enter Location"
+          />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </Box>
       </Container>
     </div>
   );
